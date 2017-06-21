cdce3fd41dfc38e106e536b0492201bc110e79a8
pacusm128 <pacusm128@gmail.com>
2017-06-21 17:30:16 +0700

    up files

---
+++ app/Http/Controllers/NationController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use App\Models\TblNation;
+
+class NationController extends Controller {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        //
+    }
+
+    public function create() {
+        //
+    }
+
+    public function show($id = null) {
+//        $nations = DB::table('tbl_nation')->get();
+       
+        $nations = TblNation::where(function($query)use($id)
+            { 
+                $query->whereRaw('nation_id = "'. $id .'"or "'. $id .'" = ""');
+            }) ->paginate(10);
+        return view('nation', ['nations' => $nations]);
+        
+    }
+    public function delete($id) {
+        //
+    }
+
+    public function update(Request $request, $id) {
+        //
+    }
+
+}
