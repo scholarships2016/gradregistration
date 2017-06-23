3756b08875ed9db9789ee5c97af092503c6de96a
worakan <worakan.pong@gmail.com>
2017-06-23 23:41:16 +0700

    Add new Register and Login Route

--- routes/web.php
+++ routes/web.php
@@ -12,35 +12,35 @@
 */
 
 Route::get('/', function () {
-   return view('index');
+    return view('index');
 });
 
-
+Route::get('login', 'LoginController@showLoginPage')->name('showLoginPage');
+Route::post('login', 'LoginController@doLogin')->name('doLogin');
+Route::post('register', 'RegisterController@doRegister')->name('doRegister');
 
 //Master
- 
+
 //Bank
- Route::any('/bank/','BankController@show');
- Route::get('/bank/bank_form/{id}','BankController@getForm');
- Route::get('/bank/bank_form/','BankController@getForm');
- Route::post('/bank/bank_form/','BankController@postForm');
- Route::get('/bank/delete/{id}','BankController@delete'); 
- 
+Route::any('/bank/', 'BankController@show');
+Route::get('/bank/bank_form/{id}', 'BankController@getForm');
+Route::get('/bank/bank_form/', 'BankController@getForm');
+Route::post('/bank/bank_form/', 'BankController@postForm');
+Route::get('/bank/delete/{id}', 'BankController@delete');
+
 //Nation
- Route::any('/nation/','NationController@show');
- Route::get('/nation/nation_form/{id}','NationController@getForm');
- Route::get('/nation/nation_form/','NationController@getForm');
- Route::post('/nation/nation_form/','NationController@postForm');
- Route::get('/nation/delete/{id}','NationController@delete');
- 
- //Degree
- Route::any('/degree/','DegreeController@show');
- Route::get('/degree/degree_form/{id}','DegreeController@getForm');
- Route::get('/degree/degree_form/','DegreeController@getForm');
- Route::post('/degree/degree_form/','DegreeController@postForm');
- Route::get('/degree/delete/{id}','DegreeController@delete'); 
- 
- 
-Auth::routes();
+Route::any('/nation/', 'NationController@show');
+Route::get('/nation/nation_form/{id}', 'NationController@getForm');
+Route::get('/nation/nation_form/', 'NationController@getForm');
+Route::post('/nation/nation_form/', 'NationController@postForm');
+Route::get('/nation/delete/{id}', 'NationController@delete');
+
+//Degree
+Route::any('/degree/', 'DegreeController@show');
+Route::get('/degree/degree_form/{id}', 'DegreeController@getForm');
+Route::get('/degree/degree_form/', 'DegreeController@getForm');
+Route::post('/degree/degree_form/', 'DegreeController@postForm');
+Route::get('/degree/delete/{id}', 'DegreeController@delete');
+
 
 
