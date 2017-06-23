a9efd0411df3e47af57dbd6a7c6252e15239ac60
worakan <worakan.pong@gmail.com>
2017-06-23 21:33:19 +0700

    Edit Bladetemplate

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,34 +1,81 @@
 @extends('layouts.default')
 
 @push('pageCss')
-<link href="{{asset('css/toastr.css')}}" rel="stylesheet">
 <style type="text/css">
 
 </style>
 @endpush
 
-@section('breadcrumb')
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item active">Dashboard</li>
-    </ol>
+@section('pagebar')
+    <div class="page-bar">
+        <ul class="page-breadcrumb">
+            <li>
+                <a href="index.html">Home</a>
+                <i class="fa fa-circle"></i>
+            </li>
+            <li>
+                <a href="#">Blank Page</a>
+                <i class="fa fa-circle"></i>
+            </li>
+            <li>
+                <span>Page Layouts</span>
+            </li>
+        </ul>
+        <div class="page-toolbar">
+            <div class="btn-group pull-right">
+                <button type="button" class="btn green btn-sm btn-outline dropdown-toggle"
+                        data-toggle="dropdown"> Actions
+                    <i class="fa fa-angle-down"></i>
+                </button>
+                <ul class="dropdown-menu pull-right" role="menu">
+                    <li>
+                        <a href="#">
+                            <i class="icon-bell"></i> Action</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="icon-shield"></i> Another action</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="icon-user"></i> Something else here</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <a href="#">
+                            <i class="icon-bag"></i> Separated link</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 @stop
 
-@section('main-content')
-    <div class="container-fluid">
-        <div class="animated fadeIn">
-            {{--{{dd($this->session)}}--}}
-            Gameeee TEsttt
-{{--            {{dd(Session::get('moduleAuths'))}}--}}
+@section('pagetitle')
+    <h1 class="page-title"> Blank Page Layout
+        <small>blank page layout</small>
+    </h1>
+@stop
+
+
+
+@section('maincontent')
+    <div class="note note-info">
+        <p> A black page template with a minimal dependency assets to use as a base for any custom page you
+            create </p>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            HelloWorld
         </div>
     </div>
 @stop
 
 
 @push('pageJs')
-<script src="{{asset('js/toastr.js')}}"></script>
 <script type="application/javascript">
     $(document).ready(function () {
-        @include('includes.toastr')
+        alert("IamReady");
     });
 </script>
 @endpush
