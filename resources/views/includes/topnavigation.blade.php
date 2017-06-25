7c507c769a367d0fbdc91987100fd95fe41ef653
worakan <worakan.pong@gmail.com>
2017-06-25 19:07:10 +0700

    Add Profile Link

--- resources/views/includes/topnavigation.blade.php
+++ resources/views/includes/topnavigation.blade.php
@@ -116,7 +116,7 @@
             </a>
             <ul class="dropdown-menu dropdown-menu-default">
                 <li>
-                    <a href="page_user_profile_1.html">
+                    <a href="{{route('profile.showProfilePage')}}">
                         <i class="icon-user"></i> My Profile </a>
                 </li>
                 <li class="divider"></li>
