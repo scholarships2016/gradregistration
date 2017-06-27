9fedc27083cc4781dc476a683732abcdbeec70a4
worakan <worakan.pong@gmail.com>
2017-06-27 22:26:36 +0700

    Edit Eng test

--- app/Repositories/EngTestRepositoryImpl.php
+++ app/Repositories/EngTestRepositoryImpl.php
@@ -4,17 +4,16 @@ namespace App\Repositories;
 
 use App\Repositories\Contracts\EngTestRepository;
 use App\Models\TblEngTest;
-use App\Utils\Util;
-use Illuminate\Support\Facades\DB;
 
-class EngTestRepositoryImpl extends AbstractRepositoryImpl implements EngTestRepository {
 
-    protected $engtestPassRepo;
+class EngTestRepositoryImpl extends AbstractRepositoryImpl implements EngTestRepository
+{
+
     private $paging = 10;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::setModelClassName(TblEngTest::class);
     }
- 
 
 }
