a47366bb4a5231d0ba55ca2cf1dc1056ab76bc28
worakan <worakan.pong@gmail.com>
2017-06-27 22:55:36 +0700

    Edit Repo Impl

--- app/Repositories/EducationPassRepositoryImpl.php
+++ app/Repositories/EducationPassRepositoryImpl.php
@@ -4,17 +4,17 @@ namespace App\Repositories;
 
 use App\Repositories\Contracts\EducationPassRepository;
 use App\Models\TblEducationPass;
-use App\Utils\Util;
-use Illuminate\Support\Facades\DB;
 
-class EducationPassRepositoryImpl extends AbstractRepositoryImpl implements EducationPassRepository {
+class EducationPassRepositoryImpl extends AbstractRepositoryImpl implements EducationPassRepository
+{
 
     protected $educationPassRepo;
     private $paging = 10;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::setModelClassName(TblEducationPass::class);
     }
- 
+
 
 }
--- app/Repositories/UniversityRepositoryImpl.php
+++ app/Repositories/UniversityRepositoryImpl.php
@@ -4,17 +4,18 @@ namespace App\Repositories;
 
 use App\Repositories\Contracts\UniversityRepository;
 use App\Models\TblUniversity;
-use App\Utils\Util;
-use Illuminate\Support\Facades\DB;
 
-class UniversityRepositoryImpl extends AbstractRepositoryImpl implements UniversityRepository {
+
+class UniversityRepositoryImpl extends AbstractRepositoryImpl implements UniversityRepository
+{
 
     protected $universityPassRepo;
     private $paging = 10;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::setModelClassName(TblUniversity::class);
     }
- 
+
 
 }
