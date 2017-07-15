b35e10b7a9360f0373013fa2883f444278635830
worakan <worakan.pong@gmail.com>
2017-07-15 17:02:21 +0700

    New File Repo

---
+++ app/Repositories/Contracts/FileRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use Illuminate\Http\UploadedFile;
+
+interface FileRepository
+{
+
+    //Generate Name
+    public function upload(UploadedFile $uploadedFile, $path = null);
+
+    public function uploadWithName(UploadedFile $uploadedFile, $newFilename, $path = null);
+
+    public function forceRemoveById($id);
+
+    public function removeToTrashById($id);
+
+    public function getContentFileById($id);
+
+    public function getImageFileAsBase64ById($id);
+}
+ 
+
+ 
