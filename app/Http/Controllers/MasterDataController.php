4d803917dbf41b6c304de30bf8e9aea3d1348932
worakan <worakan.pong@gmail.com>
2017-07-15 17:24:55 +0700

    Game Merge

--- app/Http/Controllers/MasterDataController.php
+++ app/Http/Controllers/MasterDataController.php
@@ -8,6 +8,8 @@ use App\Repositories\Contracts\NameTitleRepository;
 use App\Repositories\Contracts\NationRepository;
 use App\Repositories\Contracts\NewsSourceRepository;
 use App\Repositories\Contracts\ProvinceRepository;
+use App\Repositories\Contracts\DepartmentRepository;
+use App\Repositories\Contracts\CurriculaRepository;
 use App\Utils\Util;
 use Illuminate\Http\Request;
 
@@ -19,13 +21,16 @@ class MasterDataController extends Controller
     protected $nationRepo;
     protected $nameTitleRepo;
     protected $newSrcRepo;
+    protected $departmentRepo;
+    protected $curriculaRepo;
 
     /**
      * MasterDataController constructor.
      */
     public function __construct(DistrictRepository $districtRepo, ProvinceRepository $provinceRepo,
                                 EngTestRepository $engTestRepo, NationRepository $nationRepo,
-                                NameTitleRepository $nameTitleRepo, NewsSourceRepository $newSrcRepo)
+                                NameTitleRepository $nameTitleRepo, NewsSourceRepository $newSrcRepo,
+                                DepartmentRepository $departmentRepo, CurriculaRepository $curriculaRepo)
     {
         $this->districtRepo = $districtRepo;
         $this->provinceRepo = $provinceRepo;
@@ -33,6 +38,8 @@ class MasterDataController extends Controller
         $this->nationRepo = $nationRepo;
         $this->nameTitleRepo = $nameTitleRepo;
         $this->newSrcRepo = $newSrcRepo;
+        $this->departmentRepo = $departmentRepo;
+        $this->curriculaRepo = $curriculaRepo;
     }
 
     public function getDistrictByProvinceIdForDropdown(Request $request)
@@ -50,4 +57,37 @@ class MasterDataController extends Controller
         }
         return null;
     }
+
+    public function getDepartmentByFacultyIdForDropdown(Request $request)
+    {
+        if ($request->ajax()) {
+            $param = $request->all();
+            try {
+                if (array_key_exists('faculty_id', $param)) {
+                    $result = Util::prepareDataForDropdownList($this->departmentRepo->getByfaculty_Id($param['faculty_id']), 'department_id', 'department_name');
+                    return response()->json($result);
+                }
+            } catch (\Exception $ex) {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    public function getCurriculaByDepartmentIdForDropdown(Request $request)
+    {
+        if ($request->ajax()) {
+            $param = $request->all();
+            try {
+                if (array_key_exists('department_id', $param)) {
+                    $result = Util::prepareDataForDropdownList($this->curriculaRepo->getByDepartment_Id($param['department_id']), 'curricula_id', 'curricula_name');
+                    return response()->json($result);
+                }
+            } catch (\Exception $ex) {
+                return null;
+            }
+        }
+        return null;
+    }
+
 }
