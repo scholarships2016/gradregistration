57971d8f1c3272a02022eae0c4034f171ab9f1d4
Patipol <Patipol@DESKTOP-40FN645>
2017-07-30 22:44:45 +0700

    Merge origin/master
    Conflicts:
    	app/Models/Applicant.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -16,7 +16,7 @@
         <div class="page-header-inner ">
             <!-- BEGIN LOGO -->
             <div class="page-logo">
-                <a href="index.html">
+                <a href="{{url('home/')}}">
                     <img src="{{url('/assets/layouts/layout/img/logo.png')}}" alt="logo" class="logo-default"/> </a>
                 <div class="menu-toggler sidebar-toggler">
                     <span></span>
