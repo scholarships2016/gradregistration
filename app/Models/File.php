50346cf480bb3993da28a3b13c783d5a49ad5d7d
Patipol <Patipol@DESKTOP-40FN645>
2017-08-01 14:58:21 +0700

    Merge origin/master
    Conflicts:
    	app/Models/Applicant.php

--- app/Models/File.php
+++ app/Models/File.php
@@ -1,16 +1,58 @@
 <?php
 
+/**
+ * Created by Reliese Model.
+ * Date: Tue, 01 Aug 2017 10:44:36 +0700.
+ */
+
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Reliese\Database\Eloquent\Model as Eloquent;
 
-class File extends Model
-{
-//
-    const UPDATED_AT = 'modified';
-    const CREATED_AT = 'created';
+/**
+ * Class File
+ * 
+ * @property int $file_id
+ * @property string $file_origi_name
+ * @property string $file_gen_name
+ * @property string $file_ext
+ * @property string $file_mimetype
+ * @property string $file_path
+ * @property int $file_size
+ * @property \Carbon\Carbon $created
+ * @property \Carbon\Carbon $modified
+ * @property string $creator
+ * @property string $modifier
+ *
+ * @package App\Models
+ */
+class File extends Eloquent {
 
-    protected $primaryKey = 'file_id';
     protected $table = 'file';
-    protected $fillable = ['file_origi_name', 'file_gen_name', 'file_ext', 'file_path','file_mimetype', 'file_size', 'creator', 'modifier'];
+    protected $primaryKey = 'file_id';
+
+    const CREATED_AT = 'created';
+    const UPDATED_AT = 'modified';
+
+    public $timestamps = true;
+    protected $casts = [
+        'file_size' => 'int'
+    ];
+    protected $dates = [
+        'created',
+        'modified'
+    ];
+    protected $fillable = [
+        'file_origi_name',
+        'file_gen_name',
+        'file_ext',
+        'file_mimetype',
+        'file_path',
+        'file_size',
+        'created',
+        'modified',
+        'creator',
+        'modifier'
+    ];
+
 }
