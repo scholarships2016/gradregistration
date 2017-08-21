b3a4f0fcc3e249ef735be4e989d70474cf1f2106
worakan <worakan.pong@gmail.com>
2017-08-21 22:05:40 +0700

    Edit ApplicantManagementController

--- app/Http/Controllers/BackOffice/ApplicantManagementController.php
+++ app/Http/Controllers/BackOffice/ApplicantManagementController.php
@@ -2,23 +2,30 @@
 
 namespace App\Http\Controllers\BackOffice;
 
+use App\Repositories\Contracts\ApplicantRepository;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ApplicantManagementController extends Controller
 {
 
+    protected $applicantRepo;
 
     /**
      * ApplicantManagementController constructor.
      */
-    public function __construct()
+    public function __construct(ApplicantRepository $applicantRepo)
     {
+        $this->applicantRepo = $applicantRepo;
     }
 
     public function showManagePage(Request $request)
     {
-
+        try {
+            return view('backoffice.applicant.manage');
+        } catch (\Exception $ex) {
+            throw $ex;
+        }
     }
 
     public function showAddPage(Request $request)
@@ -29,6 +36,16 @@ class ApplicantManagementController extends Controller
     public function showEditPage(Request $request)
     {
 
+    }
+
+    public function doPaging(Request $request)
+    {
+        try {
+            $result = $this->applicantRepo->doApplicantPaging($request->all());
+            return response()->json($result);
+        } catch (\Exception $ex) {
+            throw $ex;
+        }
     }
 
 }
