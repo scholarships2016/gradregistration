bacc7bafbad21441b6d918e6dace906422b7c6c4
worakan <worakan.pong@gmail.com>
2017-08-21 20:12:35 +0700

    Add New Method [UserRepository]

--- app/Repositories/Contracts/UserRepository.php
+++ app/Repositories/Contracts/UserRepository.php
@@ -4,7 +4,13 @@ namespace App\Repositories\Contracts;
 
 interface UserRepository
 {
- public function save(array $data);
+    public function save(array $data);
+
+    public function doSave(array $data);
+
+    public function getUserPaging();
+
+    public function deleteByUserId($userId);
 }
  
 
