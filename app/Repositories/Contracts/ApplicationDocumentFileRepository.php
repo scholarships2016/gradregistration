3e28355a6f92a96b4c5c625b7bdc69535b76db09
worakan <worakan.pong@gmail.com>
2017-08-26 16:15:09 +0700

    Edit ApplicantDocFileRepo

--- app/Repositories/Contracts/ApplicationDocumentFileRepository.php
+++ app/Repositories/Contracts/ApplicationDocumentFileRepository.php
@@ -2,9 +2,14 @@
 
 namespace App\Repositories\Contracts;
 
-interface ApplicationDocumentFileRepository {
+interface ApplicationDocumentFileRepository
+{
 
- public function GetData($application_ID);
- public function DeleteNOTIN($application_ID,$doc_apply_id );
- public function saveApplicationDocumentFile($data);
+    public function GetData($application_ID);
+
+    public function DeleteNOTIN($application_ID, $doc_apply_id);
+
+    public function saveApplicationDocumentFile($data);
+
+    public function deleteByApplicationId($applicationId);
 }
