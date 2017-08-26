dffce370dd3dadbe641be067017b5185c4c97901
worakan <worakan.pong@gmail.com>
2017-08-26 16:05:50 +0700

    Add New Exception

---
+++ app/Exceptions/ApplicantDeleteException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: worakanpongnumkul
+ * Date: 8/23/2017 AD
+ * Time: 9:20 AM
+ */
+
+namespace App\Exceptions;
+
+
+class ApplicantDeleteException extends \Exception
+{
+
+}
