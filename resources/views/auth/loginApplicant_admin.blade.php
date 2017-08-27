721db663f5f40a83e1e87e12fd0ffb6338f95392
iChok@Lenovo <ichokichon@gmail.com>
2017-08-27 18:37:35 +0700

    Edit Action of Admin Login Page

--- resources/views/auth/loginApplicant_admin.blade.php
+++ resources/views/auth/loginApplicant_admin.blade.php
@@ -25,7 +25,7 @@
             <label class="control-label visible-ie8 visible-ie9">รหัสผู้ใช้</label>
             <div class="input-icon">
                 <i class="fa fa-envelope"></i>
-                <input class="form-control placeholder-no-fix"   type="text" autocomplete="off" placeholder="กรอกรหัสผู้ใช้"
+                <input class="form-control placeholder-no-fix" id="user_name"  type="text" autocomplete="off" placeholder="กรอกรหัสผู้ใช้"
                        name="user_name"/></div>
         </div>
         <div class="form-group">
@@ -33,10 +33,12 @@
             <div class="input-icon">
                 <i class="fa fa-lock"></i>
                 <input class="form-control placeholder-no-fix" type="password" autocomplete="off"
-                       placeholder="กรอกรหัสผ่าน" name="user_password"/></div>
+                       placeholder="กรอกรหัสผ่าน" id="user_password" name="user_password"/></div>
         </div>
         <div class="formm-group">
-          <div class="g-recaptcha" data-sitekey="6LdTciwUAAAAAIghJhuM4wf8Dnzc-eadlLikCWiR"></div>
+          <div class="g-recaptcha" data-sitekey="6LdTciwUAAAAAIghJhuM4wf8Dnzc-eadlLikCWiR"  data-callback="recaptchaCallback"></div>
+          <input type="hidden" class="hiddenRecaptcha required" name="hiddenRecaptcha" id="hiddenRecaptcha">
+
         </div>
         <div class="form-actions">
             <button type="submit" class="btn green pull-right"> เข้าสู่ระบบ</button>
@@ -68,5 +70,111 @@
 @push('pageJs')
 <script src="{{asset('assets/pages/scripts/login-4.min.js')}}"></script>
 <script type="application/javascript">
+
+
+
+var Login = function () {
+
+	var handleLogin = function() {
+		$('.login-form').validate({
+	            errorElement: 'span', //default input error message container
+	            errorClass: 'help-block', // default input error message class
+	            focusInvalid: false, // do not focus the last invalid input
+							ignore: ".ignore",
+	            rules: {
+	                user_name: {
+	                    required: true
+	                },
+	                user_password: {
+	                    required: true
+	                },
+									hiddenRecaptcha: {
+								                 required: function () {
+								                     if (grecaptcha.getResponse() == '') {
+								                         return true;
+								                     } else {
+								                         return false;
+								                     }
+								                 }
+ }
+	            },
+
+	            messages: {
+	                user_name: {
+	                    required: "User is required."
+	                },
+	                user_password: {
+	                    required: "Password is required."
+	                },
+	                hiddenRecaptcha: {
+	                    required: "reCAPTCHA is required."
+	                }
+	            },
+
+	            invalidHandler: function (event, validator) { //display error alert on form submit
+	                $('.alert-danger', $('.login-form')).show();
+	            },
+
+	            highlight: function (element) { // hightlight error inputs
+	                $(element)
+	                    .closest('.form-group').addClass('has-error'); // set error class to the control group
+	            },
+
+	            success: function (label) {
+	                label.closest('.form-group').removeClass('has-error');
+	                label.remove();
+	            },
+
+	            errorPlacement: function (error, element) {
+	                error.insertAfter(element.closest('.input-icon'));
+	            },
+
+	            submitHandler: function (form) {
+	                form.submit();
+	            }
+	        });
+
+	        $('.login-form input').keypress(function (e) {
+	            if (e.which == 13) {
+	                if ($('.login-form').validate().form()) {
+	                    $('.login-form').submit();
+	                }
+	                return false;
+	            }
+	        });
+	}
+
+
+
+
+    return {
+        //main function to initiate the module
+        init: function () {
+
+            handleLogin();
+
+            // init background slide images
+		    $.backstretch([
+		        "../assets/pages/media/bg/1.jpg",
+		        "../assets/pages/media/bg/2.jpg",
+		        "../assets/pages/media/bg/3.jpg",
+		        "../assets/pages/media/bg/4.jpg"
+		        ], {
+		          fade: 1000,
+		          duration: 8000
+		    	}
+        	);
+        }
+    };
+
+}();
+
+jQuery(document).ready(function() {
+    Login.init();
+});
+
+function recaptchaCallback() {
+  $('#hiddenRecaptcha').valid();
+};
 </script>
 @endpush
