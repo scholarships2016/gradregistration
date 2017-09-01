5a6a388aeb755d5d1e3393bac9767eb982d4f091
worakan <worakan.pong@gmail.com>
2017-09-02 00:56:36 +0700

    Edit BackOfficeController

--- app/Http/Controllers/BackOffice/BackOfficeController.php
+++ app/Http/Controllers/BackOffice/BackOfficeController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\BackOffice;
 
-use App\Repositories\Contracts\ApplicantRepository;
+use App\Repositories\Contracts\BackOfficeNotificationRepository;
 use App\Repositories\Contracts\CurriculumRepository;
+use App\Utils\Util;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -11,14 +12,17 @@ class BackOfficeController extends Controller
 {
     //
 
+    protected $bakOffNoticeRepo;
     protected $curriculumRepo;
 
     /**
      * BackOfficeController constructor.
      */
-    public function __construct(CurriculumRepository $curriculumRepo)
+    public function __construct(CurriculumRepository $curriculumRepo,
+                                BackOfficeNotificationRepository $bakOffNoticeRepo)
     {
         $this->curriculumRepo = $curriculumRepo;
+        $this->bakOffNoticeRepo = $bakOffNoticeRepo;
     }
 
     public function showToDoListPage(Request $request)
@@ -42,6 +46,18 @@ class BackOfficeController extends Controller
             return response()->json($this->curriculumRepo->doToDoListPaging($param));
         } catch (\Exception $ex) {
             throw $ex;
+        }
+    }
+
+    public function getWorkflowNotification(Request $request)
+    {
+        try {
+            $isAdmin = false;
+            $userId = null;
+            $result = $this->bakOffNoticeRepo->getWorkflowTaskDetailForBackOffice($isAdmin, $userId);
+            return response()->json(Util::jsonResponseFormat(1, $result, null));
+        } catch (\Exception $ex) {
+            return response()->json(Util::jsonResponseFormat(3, null, null));
         }
     }
 }
