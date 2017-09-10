7cdd9563d41be938e1acfa20c14381f5c2582f3b
iChok@Lenovo <ichokichon@gmail.com>
2017-09-10 18:23:50 +0700

    Disable filed username in case of edition

--- resources/views/backoffice/admin/edit.blade.php
+++ resources/views/backoffice/admin/edit.blade.php
@@ -59,6 +59,7 @@
                             <input type="text" name="user_name" class="form-control"
                                    placeholder="รหัสผู้ใช้ชุดเดียว Chula LDAP"
                                    value="@if(!empty($user)){{$user->user_name}}@endif"
+                                      @if(!empty($user)) disabled @endif
                             >
                         </div>
                     </div>
