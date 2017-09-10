7351f1c9797fdae53aad0b1d0b6eeb4cbc0e86d9
worakan <worakan.pong@gmail.com>
2017-09-10 20:03:10 +0700

    Edit AudittrailRepository

--- app/Repositories/Contracts/AudittrailRepository.php
+++ app/Repositories/Contracts/AudittrailRepository.php
@@ -5,4 +5,8 @@ namespace App\Repositories\Contracts;
 interface AudittrailRepository
 {
     public function save(array $data);
+
+    public function doPaging();
+
+    public function getDistinctSection();
 }
