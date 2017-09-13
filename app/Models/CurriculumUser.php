10f5e3c63815465424207506eb12c3135186e72f
iChok@Lenovo <ichokichon@gmail.com>
2017-09-13 21:00:39 +0700

    create currilumuser model

---
+++ app/Models/CurriculumUser.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ * Date: Tue, 01 Aug 2017 10:44:36 +0700.
+ */
+
+namespace App\Models;
+
+use Reliese\Database\Eloquent\Model as Eloquent;
+
+/**
+ * Class CurriculumUser
+ *
+ * @property int $user_special_id
+ * @property int $user_id
+ * @property int $doc_id
+ * @property int $doc_type
+ *
+ * @package App\Models
+ */
+class CurriculumUser extends Eloquent
+{
+	protected $table = 'curriculum_user';
+	protected $primaryKey = 'curr_user_id';
+	public $timestamps = true;
+	const CREATED_AT = 'created';
+
+	protected $casts = [
+		'curr_user_id' => 'int',
+		'curriculum_id' => 'int',
+		'user_id' => 'int'
+	];
+
+	protected $dates = [
+			'created'
+	];
+
+	protected $fillable = [
+		'curr_user_id',
+		'curriculum_id',
+		'user_id',
+		'created',
+		'creator'
+	];
+}
