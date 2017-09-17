64c25dd22b9ba5afce669a59df012af593bbc59b
worakan <worakan.pong@gmail.com>
2017-09-17 20:22:50 +0700

    edit Toast

--- resources/views/includes/toastr.blade.php
+++ resources/views/includes/toastr.blade.php
@@ -1,10 +1,10 @@
 @if(Session::has('warningMsg'))
-    var warningMsg = "{{session('warningMsg')}}";
+    var warningMsg = "{!!session('warningMsg')!!}";
     toastr.warning(warningMsg);
 @elseif(Session::has('successMsg'))
-    var successMsg = "{{session('successMsg')}}";
+    var successMsg = "{!!session('successMsg')!!}";
     toastr.success(successMsg);
 @elseif(Session::has('errorMsg'))
-    var errorMsg = "{{session('errorMsg')}}";
+    var errorMsg = "{!!session('errorMsg')!!}";
     toastr.error(errorMsg);
 @endif
