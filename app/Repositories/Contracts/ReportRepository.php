e9b63a2dbc0cbcaee5bff25a936e51638a78f424
worakan <worakan.pong@gmail.com>
2017-10-01 12:28:50 +0700

    Add ReportRepo

---
+++ app/Repositories/Contracts/ReportRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface ReportRepository
+{
+    public function getReport01DataByCriteria($criteria = null);
+
+    public function getReport02DataByCriteria($criteria = null);
+
+    public function getReport03DataByCriteria($criteria = null);
+
+}
+ 
+
+ 
