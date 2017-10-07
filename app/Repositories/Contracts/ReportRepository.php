0d7eee93936ba127d88752a6e11b5b9f76ed3233
worakan <worakan.pong@gmail.com>
2017-10-08 00:46:27 +0700

    Edit Report Repo

--- app/Repositories/Contracts/ReportRepository.php
+++ app/Repositories/Contracts/ReportRepository.php
@@ -15,7 +15,20 @@ interface ReportRepository
     public function getReport04ApplicantDataByCriteria($criteria = null);
 
     public function getFlowApplyForDropdown();
-}
- 
 
- 
+    public function getEngScoreReport($criteria = null);
+
+    public function getSatisfactionChartsData($criteria = null);
+
+    public function getSatisfactionData($criteria = null);
+
+    public function getDatasRegistrationCenter1($criteria = null);
+
+    public function getDatasRegistrationCenter2($criteria = null);
+
+    public function getDatasPolicyAndPlan($criteria = null);
+
+}
+
+
+
