31f67aa5be5b6d38439c9fbf73ad00c6b9d57762
iChok@Lenovo <ichokichon@gmail.com>
2017-11-12 00:26:12 +0700

    Edit login ldap function

--- app/Http/Controllers/Auth/loginUserController.php
+++ app/Http/Controllers/Auth/loginUserController.php
@@ -52,7 +52,7 @@ class LoginUserController extends Controller {
     }
 
     public function checkuserldap($username, $password) {
- return true;
+ //return true;
         if ($username != 'administrator' && $username != 'facultystaff' && $username != 'gradstaff') {
 
             $url = 'https://ethesis.grad.chula.ac.th/ldap/authen/get_account.php';
